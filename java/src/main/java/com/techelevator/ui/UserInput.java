8d319eba2f14451cb3dc2425b81b1c99218f5fba
Student <jess@techelevator.com>
2021-11-16 14:32:00 -0500

    initial commit

---
+++ java/src/main/java/com/techelevator/ui/UserInput.java
@@ -0,0 +1,50 @@
+package com.techelevator.ui;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+/**
+ * Responsibilities: This class should handle receiving ALL input from the User
+ * 
+ * Dependencies: None
+ */
+public class UserInput
+{
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String getHomeScreenOption()
+    {
+        System.out.println("What would you like to do?");
+        System.out.println();
+
+        System.out.println("1) Display Vending Machine Items");
+        System.out.println("2) Purchase");
+        System.out.println("3) Exit");
+
+        System.out.println();
+        System.out.print("Please select an option: ");
+
+        String selectedOption = scanner.nextLine();
+        String option = selectedOption.trim().toLowerCase();
+
+        if (option.equals("1"))
+        {
+            return "display";
+        }
+        else if (option.equals("2"))
+        {
+            return "purchase";
+        }
+        else if (option.equals("3"))
+        {
+            return "exit";
+        }
+        else
+        {
+            return "";
+        }
+
+    }
+
+    
+}
